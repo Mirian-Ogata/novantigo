9d96fc0769ba4a58400c4c705f7983f9bd3829aa
Mirian Ogata <mirianogata@gmail.com>
2024-12-26 17:37:25 -0300

    Update Produtos.tsx

--- src/components/home/produtos/Produtos.tsx
+++ src/components/home/produtos/Produtos.tsx
@@ -24,10 +24,9 @@ export default function Produtos({
   parcelas,
 }: ProdutosProps) {
   return (
-    // <div className="flex w-80 flex-col items-center justify-center bg-white text-center">
     <Link
       href={id}
-      className="flex w-full flex-col items-center justify-center gap-2 bg-white text-center text-lg font-normal text-green drop-shadow-md hover:text-acqua"
+      className="bg-z flex w-full flex-col items-center justify-center gap-2 text-center text-lg font-normal text-green drop-shadow-md hover:text-acqua"
     >
       <Image src={imagem} height={320} width={320} alt={nomeProduto} />
       <div className="flex flex-col items-center justify-center py-2">
@@ -36,6 +35,5 @@ export default function Produtos({
         <p className="text-base font-light text-brown">12x {parcelas}</p>
       </div>
     </Link>
-    // </div>
   );
 }
